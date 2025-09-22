8de50fff6a3ce5029acd92656202b40304edba12
horsenoggingit <7583259+horsenoggingit@users.noreply.github.com>
2025-09-22 07:23:48 -0700

    move clients to tab

--- HomeScript/ContentView.swift
+++ HomeScript/ContentView.swift
@@ -36,47 +36,49 @@ struct ContentView: View {
                 }
                 .listStyle(.sidebar)
                 .frame(width: 400)
-                Spacer()
-                Text("Clients")
-                List(scriptingViewModel.clients, id: \.id) { item in
-                    Text(item.id)
-                        .foregroundStyle(item.isConnected ? .green : .white)
-                }
-                .frame(width: 400, height: 200)
             }
-            VStack(alignment: .trailing) {
-                HStack {
-                    Text("Events")
-                    Spacer()
-                    Toggle(isOn: $followLastHistory) {
-                        Text("Follow New Events")
+            TabView {
+                Tab("Events", image: "") {
+                    VStack(alignment: .trailing) {
+                        HStack {
+                            Spacer()
+                            Toggle(isOn: $followLastHistory) {
+                                Text("Follow New Events")
+                            }
+                            .fixedSize()
+                        }
+                        ScrollViewReader { reader in
+                            List(viewModel.history, id: \.id) { item in
+                                VStack(alignment: .leading) {
+                                    Text(verbatim: "\(item.date?.ISO8601Format(ListItem.dateStyle) ?? "*No Time*")")
+                                        .font(Font.caption.bold())
+                                    Text(verbatim: "\(item.home)-\(item.room)-\(item.accessory)-\(item.service)-\(item.characteristic): \(item.value ?? "N/A")")
+                                }
+                            }
+                            .onChange(of: viewModel.history) { _, _ in
+                                timeoutTask?.cancel()
+                                timeoutTask = Task {
+                                    try? await Task.sleep(for:.milliseconds(200))
+                                    guard !Task.isCancelled, self.followLastHistory, let lastMessage = viewModel.history.last else { return }
+                                    withAnimation(.easeInOut(duration: 0.1)) {
+                                        reader.scrollTo(lastMessage.id, anchor: .bottom)
+                                    }
+                                }
+                            }
+                            .onChange(of: followLastHistory) { _, _ in
+                                if let lastMessage = viewModel.history.last {
+                                    withAnimation(.easeInOut(duration: 0.1)) {
+                                        reader.scrollTo(lastMessage.id, anchor: .bottom)
+                                    }
+                                }
+                            }
+                        }
                     }
-                    .fixedSize()
                 }
-                ScrollViewReader { reader in
-                    List(viewModel.history, id: \.id) { item in
-                        VStack(alignment: .leading) {
-                            Text(verbatim: "\(item.date?.ISO8601Format(ListItem.dateStyle) ?? "*No Time*")")
-                                .font(Font.caption.bold())
-                            Text(verbatim: "\(item.home)-\(item.room)-\(item.accessory)-\(item.service)-\(item.characteristic): \(item.value ?? "N/A")")
-                        }
-                    }
-                    .onChange(of: viewModel.history) { _, _ in
-                        timeoutTask?.cancel()
-                        timeoutTask = Task {
-                            try? await Task.sleep(for:.milliseconds(200))
-                            guard !Task.isCancelled, self.followLastHistory, let lastMessage = viewModel.history.last else { return }
-                            withAnimation(.easeInOut(duration: 0.1)) {
-                                reader.scrollTo(lastMessage.id, anchor: .bottom)
-                            }
-                        }
-                    }
-                    .onChange(of: followLastHistory) { _, newValue in
-                        if newValue, let lastMessage = viewModel.history.last {
-                            withAnimation(.easeInOut(duration: 0.1)) {
-                                reader.scrollTo(lastMessage.id, anchor: .bottom)
-                            }
-                        }
+                Tab("Clients", image:"") {
+                    List(scriptingViewModel.clients, id: \.id) { item in
+                        Text(item.id)
+                            .foregroundStyle(item.isConnected ? .green : .white)
                     }
                 }
             }
